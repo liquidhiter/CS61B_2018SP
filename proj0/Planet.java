103624af74829aa2a02d444e2636a832ffeb83d8
liquidhiter <909494727@qq.com>
2023-11-15 21:10:18 +0100

    Project 0 in progress

---
+++ proj0/Planet.java
@@ -0,0 +1,146 @@
+public class Planet {
+
+    /* Constant */
+    private static final double GRAVITYCONSTANT = 6.67E-11;
+
+    /* class members */
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    /*===================================== Constructors =====================================*/
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    /*===================================== calcDistance =====================================*/
+    /**
+     * 
+     * @param xPos
+     * @return
+     */
+    private double calxDistance(double xPos) {
+        return xxPos >= xPos ? xxPos - xPos : xPos - xxPos;
+    }
+
+    /**
+     * 
+     * @param yPos
+     * @return
+     */
+    private double calyDistance(double yPos) {
+        return yyPos >= yPos ? yyPos - yPos : yPos - yyPos;
+    }
+
+    /**
+     * 
+     * @param val
+     * @return
+     */
+    private double calSquare(double val) {
+        return val *= val;
+    }
+
+    /**
+     * 
+     * @param p
+     * @return
+     */
+    public double calcDistance(Planet p) {
+        assert(p != null);
+        double xDist = calxDistance(p.xxPos);
+        double yDist = calyDistance(p.yyPos);
+
+        return Math.sqrt(calSquare(xDist) + calSquare(yDist));
+    }
+
+    /*===================================== calcForceExertedBy =====================================*/
+    /**
+     * 
+     * @param p
+     * @return
+     * @invariant: always return non-negative value
+     */
+    public double calcForceExertedBy(Planet p) {
+        assert(p != null);
+        
+        double dist = calcDistance(p);
+        return GRAVITYCONSTANT * this.mass * p.mass / (dist * dist);
+    }
+
+    /*===================================== calcForceExertedByX and calcForceExertedByY =====================================*/
+    /**
+     * 
+     * @param p
+     * @return
+     */
+    public double calcForceExertedByX(Planet p) {
+        assert(p != null);
+
+        double forceExertedBy = calcForceExertedBy(p);
+        /* Duplicated calculation of the distance? */
+        double dist = calcDistance(p);
+
+        return forceExertedBy * (p.xxPos - this.xxPos) / dist;
+    }
+
+    /**
+     * 
+     * @param p
+     * @return
+     */
+    public double calcForceExertedByY(Planet p) {
+        assert(p != null);
+
+        double forceExertedBy = calcForceExertedBy(p);
+        /* Duplicated calculation of the distance? */
+        double dist = calcDistance(p);
+
+        return forceExertedBy * (p.yyPos - this.yyPos) / dist;
+    }
+
+    /*===================================== calcNetForceExertedByX  and calcNetForceExertedByY =====================================*/
+    public double calcNetForceExertedByX(Planet[] planets) {
+        double netForxtX = 0.0;
+        for (Planet p : planets) {
+
+            /*Planets cannot exert gravitational forces on themselves!*/
+            if (p.equals(this)) {
+                continue;
+            }
+            netForxtX += calcForceExertedByX(p);
+        }
+        return netForxtX;
+    }
+
+    public double calcNetForceExertedByY(Planet[] planets) {
+        double netForxtY = 0.0;
+        for (Planet p : planets) {
+
+            /*Planets cannot exert gravitational forces on themselves!*/
+            if (p.equals(this)) {
+                continue;
+            }
+            netForxtY += calcForceExertedByY(p);
+        }
+        return netForxtY;
+    }
+
+}
