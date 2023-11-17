4df0b72e3308d0f76d4313de5be4d11af5515943
liquidhiter <909494727@qq.com>
2023-11-18 00:02:03 +0100

    Finish project 0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -4,6 +4,7 @@ public class NBody {
     private static double inDt;
     private static String inDataFilePath;
     public static final String BACKGROUND_IMG = "./images/starfield.jpg";
+    private static final int PAUSE_INTERVAL = 10; /* milliseconds */
 
     /**
      * 
@@ -18,7 +19,12 @@ public class NBody {
         in.readInt();
         
         /* read radius from the file */
-        return in.readDouble();
+        double radius = in.readDouble();
+    
+        /* release the file handler */
+        in.close();
+
+        return radius;
     }
 
     /**
@@ -57,6 +63,9 @@ public class NBody {
                 foundPlanets[i] = newPlanet;
         }
 
+        /* close the file handler */
+        in.close();
+
         return foundPlanets;
     }
 
@@ -88,40 +97,125 @@ public class NBody {
 
     /**
      * 
+     * @param radius
      */
-    private static void drawBackground() {
-        /* Get the radius of the universe */
-        double radius = readRadius(inDataFilePath);
-        assert(radius > 0);
+    private static void drawBackground(double radius) {
         StdDraw.setScale(-radius,radius);
-        StdDraw.clear();
         StdDraw.picture(0, 0, BACKGROUND_IMG);
-
-        /* Shows the drawing to the screen, and waits 2000 milliseconds. */
-        StdDraw.show();
-        StdDraw.pause(3000);	
     }
 
     /**
      * 
      */
-    private static void drawPlanets() {
-        Planet[] planets = readPlanets(inDataFilePath);
-        assert(planets.length > 0);
-
+    private static void drawPlanets(Planet[] planets) {
         for (Planet planet : planets ) {
             planet.draw();
         }
+    }
+
+    /**
+     * 
+     * @param xForces
+     * @param yForces
+     */
+    private static void updateNetForces(double[] xForces, double[] yForces, Planet[] planets) {
+        /* Sanity check */
+        assert(xForces != null);
+        assert(yForces != null);
+        assert(planets != null);
+
+        /* Invarint: index of net force keeps the same as the index of planet */
+        int numOfPlanets = planets.length;
+        for (int i = 0; i < numOfPlanets; ++i) {
+            xForces[i] = planets[i].calcNetForceExertedByX(planets);
+            yForces[i] = planets[i].calcNetForceExertedByY(planets);
+        }
+    }
+
+    /**
+     * 
+     * @param planets
+     * @param xForces
+     * @param yForces
+     * @param second
+     */
+    private static void updatePlanets(Planet[] planets, double[] xForces, double[] yForces, double second) {
+        /* Sanity check */
+        int numOfPlanets = planets.length;
+        /* Assumption: index of net force keeps the same as the index of planet */
+        for (int i = 0; i < numOfPlanets; ++i) {
+            planets[i].update(second, xForces[i], yForces[i]);
+        }
+    }
+
+    /**
+     * 
+     * @param universe
+     */
+    private static void printUniverse(Planet[] universe, double radius) {
+        assert(universe != null);
+        StdOut.printf("%d\n", universe.length);
+        StdOut.printf("%.2e\n", radius);
+        for (Planet p : universe) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n", p.xxPos, p.yyPos, p.xxVel, p.yyVel, p.mass, p.imgFileName);
+        }
+    }
+
+    /**
+     * 
+     */
+    private static void startSimu() {
+
+        /* First get planets in the current unviverse */
+        Planet[] planets = readPlanets(inDataFilePath);
+        assert(planets.length > 0);
+
+        /* Get the radius of the universe */
+        double radius = readRadius(inDataFilePath);
+        assert(radius > 0);
+
+        /* Enable the buffering globally */
+        StdDraw.enableDoubleBuffering();
+
+        /* Clear the window */
+        StdDraw.clear();
+
+        /* Container storing the net forces */
+        double[] xForces = new double[planets.length];
+        double[] yForces = new double[planets.length];
+
+        double timer = 0.0;
+        while ((inT - timer) >= inDt) {
+
+            /* Calculate the net forces */
+            updateNetForces(xForces, yForces, planets);
+
+            /* Update planets status with new forces ? */
+            updatePlanets(planets, xForces, yForces, inDt);
+
+            /* draw the background */
+            drawBackground(radius);
+
+            /* draw all planets */
+            drawPlanets(planets);
+
+            /* show the screen buffer */
+            StdDraw.show();
+            StdDraw.pause(PAUSE_INTERVAL);
+
+            /* increase timer by interval specified by inDt */
+            timer += inDt;
+        }
+
+        /* Print the universe as you want */
+        printUniverse(planets, radius);
     }
 
     public static void main(String[] args) {
         /* Get input from the command line arguments */
         getInput(args);
 
-        /* Draw the background */
-        drawBackground();
-
-        /* Draw planets */
-        drawPlanets();
+        /* Lets simulate the universe!!! */
+        startSimu();
     }
 }
