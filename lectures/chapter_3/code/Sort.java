0de34e39ac9f3ef4501bbf6ae82cb9cb5fceefdf
liquidhiter <909494727@qq.com>
2023-11-18 10:17:37 +0100

    Implement selection sort

--- lectures/chapter_3/code/Sort.java
+++ lectures/chapter_3/code/Sort.java
@@ -1,6 +1,59 @@
 public class Sort {
-    
-    public static void sort(String[] strs) {
 
+    /**
+     *
+     * @param str
+     * @param from
+     * @param to
+     */
+    private static void selectionSort(String[] str, int from, int to) {
+        if (from == to) {
+            return;
+        }
+
+        int indexOfSmallest = findSmallest(str, from, to);
+        swap(str, from, indexOfSmallest);
+
+        selectionSort(str, from + 1, to);
+    }
+
+    /**
+     * Sort strings destructively
+     * @param str
+     */
+    public static void sort(String[] str) {
+        /* 1> Find the smallest item
+         * 2> Move it to the front
+         * 3> Selection sort the rest
+         */
+        selectionSort(str, 0, str.length);
+    }
+
+    /**
+     * Return the smallest string in the given array
+     * @param str
+     * @return
+     */
+    public static int findSmallest(String[] str, int from, int to) {
+        int smallest = from;
+        for (int i = from; i < to; ++i) {
+            if (str[i].compareTo(str[smallest]) < 0) {
+                smallest = i;
+            }
+        }
+
+        return smallest;
+    }
+
+    /**
+     *
+     * @param str
+     * @param a
+     * @param b
+     */
+    public static void swap(String[] str, int a, int b) {
+       String tmp = str[a];
+       str[a] = str[b];
+       str[b] = tmp;
     }
 }
--- lectures/chapter_3/code/TestSort.java
+++ lectures/chapter_3/code/TestSort.java
@@ -10,7 +10,23 @@ public class TestSort {
         org.junit.Assert.assertArrayEquals(expected, input);
     }
 
+    public static void testFindSmallest() {
+        String[] input = {"i", "have", "an", "egg"};
+        int expected = 2;
+        int actual = Sort.findSmallest(input, 0, input.length);
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    public static void testSwap() {
+        String[] input = {"i", "have", "an", "egg"};
+        String[] expected = {"an", "have", "i", "egg"};
+        Sort.swap(input, 0, 2);
+        org.junit.Assert.assertArrayEquals(expected, input);
+    }
+
     public static void main(String[] args) {
+        testFindSmallest();
+        testSwap();
         testSort();
     }
 }
