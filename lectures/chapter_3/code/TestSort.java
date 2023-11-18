ae932b70c292e614606e2badd577edbbf3f16752
liquidhiter <909494727@qq.com>
2023-11-18 10:32:11 +0100

    Simplify Junit use

--- lectures/chapter_3/code/TestSort.java
+++ lectures/chapter_3/code/TestSort.java
@@ -1,32 +1,29 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestSort {
-    
-    public static void testSort() {
+
+    @Test
+    public void testSort() {
         String[] input = {"i", "have", "an", "egg"};
         String[] expected = {"an", "egg", "have", "i"};
         Sort.sort(input);
-        org.junit.Assert.assertArrayEquals(expected, input);
+        assertArrayEquals(expected, input);
     }
 
-    public static void testFindSmallest() {
+    @Test
+    public void testFindSmallest() {
         String[] input = {"i", "have", "an", "egg"};
         int expected = 2;
         int actual = Sort.findSmallest(input, 0, input.length);
-        org.junit.Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
-    public static void testSwap() {
+    @Test
+    public void testSwap() {
         String[] input = {"i", "have", "an", "egg"};
         String[] expected = {"an", "have", "i", "egg"};
         Sort.swap(input, 0, 2);
-        org.junit.Assert.assertArrayEquals(expected, input);
-    }
-
-    public static void main(String[] args) {
-        testFindSmallest();
-        testSwap();
-        testSort();
+        assertArrayEquals(expected, input);
     }
 }
