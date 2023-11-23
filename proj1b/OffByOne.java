fdbac39ad380e6327ac0e145e1ad36fca9a902d2
liquidhiter <909494727@qq.com>
2023-11-23 06:45:40 +0100

    Uncomment some test cases

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -7,6 +7,6 @@ public class OffByOne implements CharacterComparator {
      */
     @Override
     public boolean equalChars(char x, char y) {
-        return x - y == 1 || x - y == -1;
+        return x - y == 1 || y - x == 1;
     }
 }
