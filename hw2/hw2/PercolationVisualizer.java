caacc2cd3bed21022b54f9c17f154a39287d855b
liquidhiter <909494727@qq.com>
2024-04-08 21:30:43 +0200

    Complete chapter 10

--- hw2/hw2/PercolationVisualizer.java
+++ hw2/hw2/PercolationVisualizer.java
@@ -22,7 +22,7 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class PercolationVisualizer {
-/*
+
     // delay in miliseconds (controls animation speed)
     private static final int DELAY = 100;
 
@@ -81,7 +81,8 @@ public class PercolationVisualizer {
     }
 
     public static void main(String[] args) {
-        String filename = args[0];
+        // String filename = args[0];
+        String filename = "/home/maxin/Git/CS61B_2018SP/hw2/inputFiles/snake101.txt";
         simulateFromFile(filename);
-    }*/
+    }
 }
